964932f4fcb8f7e23cbb1142cb14b57ae1f06ee7
Clinton Law <Klintonlaww@gmail.com>
2022-02-04 23:24:32 +0100

    selector approach

--- 06-questions-app/app.js
+++ 06-questions-app/app.js
@@ -1,11 +1,20 @@
 //using selectors inside the element
 // traversing the dom
 
-const btns = document.querySelectorAll(".question-btn");
+const questions = document.querySelectorAll(".question");
 
-btns.forEach(function (btn) {
-  btn.addEventListener("click", function (e) {
-    const question = e.currentTarget.parentElement.parentElement;
-    question.classList.toggle("show-text");
+questions.forEach((eachQuestion) => {
+  const btn = eachQuestion.querySelector(".question-btn");
+  btn.addEventListener("click", () => {
+    eachQuestion.classList.toggle("show-text");
   });
 });
+
+// const btns = document.querySelectorAll(".question-btn");
+
+// btns.forEach(function (btn) {
+//   btn.addEventListener("click", function (e) {
+//     const question = e.currentTarget.parentElement.parentElement;
+//     question.classList.toggle("show-text");
+//   });
+// });
