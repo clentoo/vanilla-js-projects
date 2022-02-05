d5a9381c6c27f5c0e0beab6fe19d6a2c1321eaad
Clinton Law <Klintonlaww@gmail.com>
2022-02-05 23:46:34 +0100

    single open element functionality

--- 06-questions-app/app.js
+++ 06-questions-app/app.js
@@ -6,6 +6,12 @@ const questions = document.querySelectorAll(".question");
 questions.forEach((eachQuestion) => {
   const btn = eachQuestion.querySelector(".question-btn");
   btn.addEventListener("click", () => {
+    questions.forEach((item) => {
+      if (item !== eachQuestion) {
+        item.classList.remove("show-text");
+      }
+    });
+
     eachQuestion.classList.toggle("show-text");
   });
 });
