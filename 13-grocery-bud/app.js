cddacccbf6280f4cf53190f3df5092d1208856f8
Clinton Law <Klintonlaww@gmail.com>
2022-03-05 22:53:01 +0100

    get local storage functionality

--- 13-grocery-bud/app.js
+++ 13-grocery-bud/app.js
@@ -136,15 +136,19 @@ function setBackToDefault() {
 // ****** LOCAL STORAGE **********
 function addToLocalStorage(id, value) {
   const grocery = { id, value };
-  let items = localStorage.getItem("list")
-    ? JSON.parse(localStorage.getItem("list"))
-    : [];
+  let items = getLocalStorage();
   console.log(items);
-  items.push(grocery)
-  localStorage.setItem('list', JSON.stringify(items))
+  items.push(grocery);
+  localStorage.setItem("list", JSON.stringify(items));
 }
 
 function removeFromLocalStorage(id) {}
 
 function editLocalStorage(id, value) {}
+
+function getLocalStorage() {
+  return localStorage.getItem("list")
+    ? JSON.parse(localStorage.getItem("list"))
+    : [];
+}
 // ****** SETUP ITEMS **********
