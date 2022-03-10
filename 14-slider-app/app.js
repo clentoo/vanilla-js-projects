5f5e6f66fc56a8ad814d87c7893808c50fecec07
Clinton Law <Klintonlaww@gmail.com>
2022-03-10 23:14:14 +0100

    prev and next functionality complete

---
+++ 14-slider-app/app.js
@@ -0,0 +1,25 @@
+const slides = document.querySelectorAll(".slide");
+const nextBtn = document.querySelector(".nextBtn");
+const prevBtn = document.querySelector(".prevBtn");
+
+slides.forEach((slide, index) => {
+  slide.style.left = `${index * 100}%`;
+});
+
+let counter = 0;
+
+nextBtn.addEventListener("click", () => {
+  counter++;
+  carousel();
+});
+
+prevBtn.addEventListener("click", () => {
+  counter--;
+  carousel();
+});
+
+function carousel() {
+  slides.forEach((slide) => {
+    slide.style.transform = `translateX(-${counter * 100}%)`;
+  });
+}
